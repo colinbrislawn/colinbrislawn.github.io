83d48a0f52fb3127263e71096033fd411c0b7bcd
Colin Brislawn <cbrisl@gmail.com>
2022-06-14 13:15:25 -0400

    major updates

--- 2_make_and_push.bat
+++
@@ -1,5 +0,0 @@
-pelican content --output output --settings publishconf.py
-cd output
-git add --all
-git commit -m "commit and push all"
-git push origin master --force
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
-AUTHOR = u'Colin Brislawn'
-SITENAME = u'Colin Brislawn'
-SITEURL = 'http://colinbrislawn.github.io'
+AUTHOR = 'Colin Brislawn'
+SITENAME = 'Colin Brislawn'
+SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'America/Los_Angeles'
 
-DEFAULT_LANG = u'en'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -29,16 +25,19 @@ LINKS = (
 # Social widget
 SOCIAL = (
 	('GitHub', 'https://github.com/colinbrislawn'),
-	('Google+', 'https://plus.google.com/+ColinBrislawn'),
 	('Linkedin', 'https://www.linkedin.com/pub/colin-brislawn/a3/233/7'),
-	('Not on Facebook', 'https://www.facebook.com/pages/King-Colin-Brislawn/216922768365881'),
+	('Google Scholar', 'https://scholar.google.com/citations?user=fyhedCwAAAAJ&hl=en'),
+	('ORCID', 'https://orcid.org/0000-0002-9109-1950'),
+	# ('Not on Facebook', 'https://www.facebook.com/pages/King-Colin-Brislawn/216922768365881'),
 	)
 
 DEFAULT_PAGINATION = 5
 
+STATIC_PATHS = ['images', 'pages', 'extra/CNAME']
+
+# THEME = '../pelican-themes/simple-bootstrap'
+# THEME = '../CamerataMusica/pelican-bootstrap3' # works, lol
+THEME = 'theme'
+
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
-THEME = "../pelican-themes/pelican-bootstrap3"
-
-PLUGIN_PATHS = ['../pelican-plugins']
-#PLUGINS=['sitemap',]
